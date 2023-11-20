7dbd75a9e3d552057743a90945dd3147701783f3
Naresh <99949786+NareshGarfield@users.noreply.github.com>
2023-11-20 16:34:07 +0530

    Updated WorkSlider.js

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -77,18 +77,23 @@ const WorkSlider = () => {
                   <div className='absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0
                   group-hover:opacity-80 transition-all duration-700'>
                   </div>
+        
                   <div className='absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20
                   transition-all duration-400 text-[13px] tracking-[0.2em]  '><div className='text-lg font-semibold '>
                     <a key={index} href={image.link} target='_blank'>
-                    {image.title}</a></div>
+                    {image.title}</a>
+                    </div>
+                           
                     <div className='flex items-center gap-x-2'>                     
                         <div className='delay-100'><a key={index} href={image.link} target='_blank'>
                           Live </a>
                           </div>
+                           
                         <div className='translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150'>
                         <a key={index} href={image.link} target='_blank'>
                           Project </a>
                           </div>
+                           
                         <div className='text-xl translate-y-[500%] group-hover:translate-y-0 transition-all 
                         duration-300 delay-200'>
                           <a key={index} href={image.link} target='_blank'>
